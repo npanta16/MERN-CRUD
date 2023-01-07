faf6677f11c546fd7591135736d926ab923da19b
npanta16 <npanta16@gmail.com>
2023-01-07 22:41:37 +1100

    Simple MERN stack project completed with CRUD operation

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,3 +1,4 @@
+import { response } from 'express';
 import User from '../schema/user-schema.js';
 
 
@@ -18,3 +19,41 @@ export const addUser =async(request, response)=>{
 
 }
 
+export const getUsers = async(request, response)=>{
+    try{
+        const users = await User.find({});
+        response.status(200).json(users);
+    }catch(error){
+        response.status(404).json({message: error.message});
+    }
+}
+
+export const getUser = async (req, res)=>{
+    try{
+        const user = await User.findById(req.params.id);
+        res.status(200).json(user);
+    }catch(error){
+        res.status(404).json({message: error.message})
+    }
+}
+
+export const editUser = async(request, response) =>{
+    let user = request.body;
+    const editUser = new User(user);
+    try{
+
+        await User.updateOne({_id:request.params.id}, editUser)
+        response.status(201).json(editUser);
+    }catch(error){
+        response.stats(409).json({message: error.message})
+    }
+}
+
+export const deleteUser=async(request, response)=>{
+    try{
+        await User.deleteOne({_id: request.params.id});
+        response.status(200).json({message:'user deleted successfully'});
+    }catch(error){
+        response.status(409).json({message:error.message});
+    }
+}
